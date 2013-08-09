a2f47eda9d6d7325abd408db00f7b9019399cdd2
John Tolly <johnftolly@gmail.com>
2013-08-08 21:00:33 -0400

    added cifs utils install

--- apartment/ubuntu-apartment.sh
+++ apartment/ubuntu-apartment.sh
@@ -1,12 +1,16 @@
+sudo apt-get update
+sudo apt-get install -y cifs-utils
+
+
 #Mounting of the Drobo
 sudo echo -e "//192.168.2.45/Media /home/tolly/Drobo	cifs	username=Admin,password=Pass@word1,uid=tolly,gid=tusers,dir_mode=0774,file_mode=0774	0	0" >> /etc/fstab
 sudo echo -e "#Above is the primary mount for Drobo and the default credentials    ">> /etc/fstab
-mkdir home/tolly/Drobo
+mkdir /home/tolly/Drobo
 sudo mount -a
 
 #Installing software
-sudo apt-get update
-sudo apt-get upgrade -y
+#sudo apt-get update
+#sudo apt-get upgrade -y
 sudo apt-get install -y xbmc sysstat iotop ssh gparted curl
 
 
