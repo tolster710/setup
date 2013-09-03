85724241afdadff681c7a1cdda207003a45e935a
tolster710 <johnftolly@gmail.com>
2013-09-03 00:12:01 -0400

    First version of autorun script, really nothing yet

---
+++ apartment/autorun.sh
@@ -0,0 +1,6 @@
+# Script to be run on interval by host systems, for either collecting logs, or running restart or cleanup operations
+
+#Version A 0 initial 03Sept2013
+
+# restart XBMC , system services
+
