f55382b81df9bc576e36a4721ca1b2ef11e22f1a
tolster710 <johnftolly@gmail.com>
2013-09-12 22:05:07 -0400

    adding cleanup files

--- apartment/cron/trenort_cleanup.sh
+++ apartment/cron/trenort_cleanup.sh
@@ -6,10 +6,32 @@ mv $f $t && echo "moved $f -> $t" >> /home/tolly/Drobo/Trenort/rename.log;
 done
 
 for f in `find . -name "*HDTV*"`;
-do t=`echo $f | sed 's/HDTV.//g' | sed 's/x264//g' | sed 's/720p//g' | sed 's/EVOLVE//g' sed 's/ASAP//g' | sed 's/.-//g' `;
-echo "$t";
-done
-
+do t=`echo $f | 
+sed 's/HDTV.//g' | 
+sed 's/x264//g' |
+ sed 's/720p//g' | 
+sed 's/EVOLVE//g' | 
+sed 's/ASAP//g' | 
+sed 's/IMMERSE//g' |
+ sed 's/LOL//g' | 
+sed 's/KILLERS//g' |
+ sed 's/REPACK//g' |
+ sed 's/X26DIMENSION//g' | 
+sed 's/AAC//g' | 
+sed 's/MVGroup//g' | 
+sed 's/\[VTV\]//g' |
+sed 's/\[ettv\]//g' |
+sed 's/XviAFG//g' |
+ sed 's/DHD//g' |  
+sed 's/480p//g' | 
+sed 's/hdtv//g' |
+sed 's/2hd//g' |
+sed 's/x264//g' |
+sed 's/\-//g'  |
+sed 's/\.\./\./g' |
+sed 's/\.\./\./g' |
+sed 's/\.\./\./g'
+`;
 
 mv $f $t && echo "moved $f -> $t" >> /home/tolly/Drobo/Trenort/shorten.log;
 done
@@ -33,8 +55,10 @@ find . -name "*.avi" -exec mv {} .\;
 find . -name "*.m4v" -exec mv {} .\;
 find . -name "*sample.*" -exec rm {} \;
 find . -name "Sample/*" -exec rm {} \;
+find . -name "sample-*" -exec rm {} \;
 find . -type d -name "Sample" -exec rmdir {} \;
 find . -type d -exec rmdir {} \;
+
 
 
 
